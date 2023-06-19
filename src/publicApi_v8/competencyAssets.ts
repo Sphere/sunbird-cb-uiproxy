09c7ad8b06c4b16cf140b4288ef0a91013dae96d
Sumit Bajaj <sumit.bajaj@tarento.com>
2023-06-19 11:46:43 +0530

    lint fix

--- src/publicApi_v8/competencyAssets.ts
+++ src/publicApi_v8/competencyAssets.ts
@@ -1,42 +1,42 @@
-import axios from "axios";
-import { Router } from "express";
-import { CONSTANTS } from "../utils/env";
-export const competencyAssets = Router();
-competencyAssets.get("/roleWiseCompetencyData", async (_req, res) => {
+import axios from 'axios'
+import { Router } from 'express'
+import { CONSTANTS } from '../utils/env'
+export const competencyAssets = Router()
+competencyAssets.get('/roleWiseCompetencyData', async (_req, res) => {
   try {
-    const filePath = CONSTANTS.COMPETENCY_ROLES_DATA_PATH;
+    const filePath = CONSTANTS.COMPETENCY_ROLES_DATA_PATH
     const response = await axios({
       headers: {},
-      method: "GET",
+      method: 'GET',
       url: filePath,
-    });
+    })
     res.status(200).json({
-      status: 200,
       response: response.data,
-    });
+      status: 200,
+    })
   } catch (err) {
     res.status(404).json({
+      message: 'Error while competency data fetch',
       status: 404,
-      message: "Error while competency data fetch",
-    });
+    })
   }
-});
-competencyAssets.get("/rolesMappingData", async (_req, res) => {
+})
+competencyAssets.get('/rolesMappingData', async (_req, res) => {
   try {
-    const filePath = CONSTANTS.COMPETENCY_ROLES_MAPPING_PATH;
+    const filePath = CONSTANTS.COMPETENCY_ROLES_MAPPING_PATH
     const response = await axios({
       headers: {},
-      method: "GET",
+      method: 'GET',
       url: filePath,
-    });
+    })
     res.status(200).json({
-      status: 200,
       response: response.data,
-    });
+      status: 200,
+    })
   } catch (err) {
     res.status(404).json({
+      message: 'Error while competency mapping fetch ',
       status: 404,
-      message: "Error while competency mapping fetch ",
-    });
+    })
   }
-});
+})
