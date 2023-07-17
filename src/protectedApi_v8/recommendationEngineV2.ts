1236028d2d017baedb188ff9a9d3e77ae4c74641
Sumit Bajaj <sumit.bajaj@tarento.com>
2023-07-17 13:24:22 +0530

    recommendation API changed

--- src/protectedApi_v8/recommendationEngineV2.ts
+++ src/protectedApi_v8/recommendationEngineV2.ts
@@ -1,35 +1,39 @@
-import axios from 'axios'
-import { Router } from 'express'
-import { CONSTANTS } from '../utils/env'
-import { logInfo } from '../utils/logger'
+import axios from "axios";
+import { Router } from "express";
+import { CONSTANTS } from "../utils/env";
+import { logInfo } from "../utils/logger";
 
 const API_END_POINTS = {
   // tslint:disable-next-line: no-any
   recommendationAPI: `${CONSTANTS.RECOMMENDATION_API_BASE_V2}/recom/`,
-}
-const unknownError = 'Failed due to unknown reason'
+};
+const unknownError = "Failed due to unknown reason";
 
-export const recommendationEngineV2 = Router()
-recommendationEngineV2.get('/', async (req, res) => {
+export const recommendationEngineV2 = Router();
+recommendationEngineV2.get("/", async (req, res) => {
   try {
-    let params = 'profession'
-    if (req.query.background) {
-      params = 'background'
+    if (!req.query.background || !req.query.profession) {
+      res.status(400).json({
+        message: "Background or profession missing",
+      });
     }
-    logInfo(params, 'params from recommendation engine')
+    logInfo(req.query.params, "params from recommendation engine");
     /* tslint:disable-next-line */
     const response = await axios({
-      method: 'GET',
-      params: { [params]: req.query[params] },
-      url: API_END_POINTS.recommendationAPI + params,
-    })
-    logInfo(response.data, 'response from recommendation engine')
-    res.status(response.status).send(response.data)
+      method: "GET",
+      params: {
+        background: req.query.background,
+        profession: req.query.profession,
+      },
+      url: API_END_POINTS.recommendationAPI,
+    });
+    logInfo(response.data, "response from recommendation engine");
+    res.status(response.status).send(response.data);
   } catch (err) {
     res.status((err && err.response && err.response.status) || 500).send(
       (err && err.response && err.response.data) || {
         error: unknownError,
       }
-    )
+    );
   }
-})
+});
