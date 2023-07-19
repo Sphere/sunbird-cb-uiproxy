be536a652188fdb9adc0613a8ff3cbf34c889869
Sumit Bajaj <sumit.bajaj@tarento.com>
2023-07-19 12:54:53 +0530

    lint fix

--- src/protectedApi_v8/recommendationEngineV2.ts
+++ src/protectedApi_v8/recommendationEngineV2.ts
@@ -1,42 +1,40 @@
-import axios from "axios";
-import { Router } from "express";
-import { CONSTANTS } from "../utils/env";
-import { logInfo } from "../utils/logger";
+import axios from 'axios'
+import { Router } from 'express'
+import { CONSTANTS } from '../utils/env'
+import { logInfo } from '../utils/logger'
 
 const API_END_POINTS = {
   // tslint:disable-next-line: no-any
   recommendationAPI: `${CONSTANTS.RECOMMENDATION_API_BASE_V2}/course/recommendation`,
-};
-const unknownError = "Failed due to unknown reason";
+}
+const unknownError = 'Failed due to unknown reason'
 
-export const recommendationEngineV2 = Router();
-recommendationEngineV2.get("/", async (req, res) => {
+export const recommendationEngineV2 = Router()
+recommendationEngineV2.get('/', async (req, res) => {
   try {
     /* tslint:disable-next-line */
     let responseObject = {
       background: req.query.background,
       profession: req.query.profession,
-    };
+    }
     if (!req.query.background) {
-      delete responseObject.background;
+      delete responseObject.background
     }
     if (!req.query.profession) {
-      delete responseObject.profession;
+      delete responseObject.profession
     }
-    console.log(responseObject);
     const response = await axios({
-      method: "GET",
+      method: 'GET',
       params: responseObject,
       url: API_END_POINTS.recommendationAPI,
-    });
-    logInfo(response.data, "response from recommendation engine");
-    res.status(response.status).send(response.data);
+    })
+    logInfo(response.data, 'response from recommendation engine')
+    res.status(response.status).send(response.data)
   } catch (err) {
-    console.log(err);
     res.status((err && err.response && err.response.status) || 500).send(
       (err && err.response && err.response.data) || {
         error: unknownError,
       }
-    );
+    )
   }
-});
+})
