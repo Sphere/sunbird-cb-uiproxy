65ac519de27a24fc5e5caf75b975025f214a703f
Sumit Bajaj <sumit.bajaj@tarento.com>
2023-08-08 13:32:31 +0530

    fix added

--- src/protectedApi_v8/creatorCertificateTemplate.ts
+++ src/protectedApi_v8/creatorCertificateTemplate.ts
@@ -1,42 +1,47 @@
-import axios from 'axios'
-import express from 'express'
-import { CONSTANTS } from '../utils/env'
-import { logInfo } from '../utils/logger'
-import { extractUserToken } from '../utils/requestExtract'
+import axios from "axios";
+import express from "express";
+import { CONSTANTS } from "../utils/env";
+import { logInfo } from "../utils/logger";
+import { extractUserToken } from "../utils/requestExtract";
+import { Logic } from "keycloak-admin/lib/defs/policyRepresentation";
 
-export const creatorCertificateTemplate = express.Router()
-const templateAddEndpoint = `${CONSTANTS.HTTPS_HOST}/api/course/batch/cert/v1/template/add`
-creatorCertificateTemplate.patch('/template/add', async (req, res) => {
+export const creatorCertificateTemplate = express.Router();
+const templateAddEndpoint = `${CONSTANTS.HTTPS_HOST}/api/course/batch/cert/v1/template/add`;
+creatorCertificateTemplate.patch("/template/add", async (req, res) => {
   try {
-    const templateBody = req.body.request.batch
-    const courseId = templateBody.courseId
-    const batchId = templateBody.batchId
-    const template = templateBody.template
+    const templateBody = req.body.request.batch;
+    logInfo("URL", templateAddEndpoint);
+    logInfo("template body", JSON.stringify(templateBody));
+    const courseId = templateBody.courseId;
+    const batchId = templateBody.batchId;
+    const template = templateBody.template;
     if (!courseId || !batchId || !template) {
       res.status(400).json({
-        message: 'Either courseId, batchId, template missing',
-        status: 'FAILED',
-      })
-      return
+        message: "Either courseId, batchId, template missing",
+        status: "FAILED",
+      });
+      return;
     }
     const templateAddResponse = await axios({
       data: req.body,
       headers: {
         Authorization: CONSTANTS.SB_API_KEY,
-        'x-authenticated-user-token': extractUserToken(req),
+        "x-authenticated-user-token": extractUserToken(req),
       },
-      method: 'PATCH',
+      method: "PATCH",
       url: templateAddEndpoint,
-    })
-    logInfo('templateAddResponse', JSON.stringify(templateAddResponse))
+    });
+    logInfo("add response", JSON.stringify(templateAddResponse.data));
+    logInfo("templateAddResponse", JSON.stringify(templateAddResponse));
     res.status(200).json({
-      message: 'SUCCESS',
+      message: "SUCCESS",
       response: templateAddResponse.data,
-    })
+    });
   } catch (error) {
+    logInfo(JSON.stringify(error));
     res.status(400).json({
-      message: 'FAILED',
-      response: 'Error occurred while template add',
-    })
+      message: "FAILED",
+      response: "Error occurred while template add",
+    });
   }
-})
+});
