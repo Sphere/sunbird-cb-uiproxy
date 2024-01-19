2552942653f4c4a573c2dbb1919ada051104df57
Sumit Bajaj <sumit.bajaj@tarento.com>
2024-01-19 08:31:49 +0530

    course recommendation search API added

--- src/protectedApi_v8/recommendationEngineV2.ts
+++ src/protectedApi_v8/recommendationEngineV2.ts
@@ -6,6 +6,8 @@ import { logInfo } from '../utils/logger'
 const API_END_POINTS = {
   // tslint:disable-next-line: no-any
   recommendationAPI: `${CONSTANTS.RECOMMENDATION_API_BASE_V2}/course/recommendation`,
+  searchAPI: `${CONSTANTS.RECOMMENDATION_API_BASE_V2}/publicSearch/getcourse`,
+
 }
 const unknownError = 'Failed due to unknown reason'
 
@@ -27,6 +29,29 @@ recommendationEngineV2.get('/', async (req, res) => {
       method: 'GET',
       params: responseObject,
       url: API_END_POINTS.recommendationAPI,
+    })
+    res.status(response.status).send(response.data)
+  } catch (err) {
+    logInfo(JSON.stringify(err))
+    res.status((err && err.response && err.response.status) || 500).send(
+      (err && err.response && err.response.data) || {
+        error: unknownError,
+      }
+    )
+  }
+})
+recommendationEngineV2.post('/publicSearch/getcourse', async (req, res) => {
+  try {
+    logInfo('Inside recommendation search course route')
+    /* tslint:disable-next-line */
+    let searchRequestBody = req.body
+    const response = await axios({
+      data: searchRequestBody,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      url: API_END_POINTS.searchAPI,
     })
     res.status(response.status).send(response.data)
   } catch (err) {
