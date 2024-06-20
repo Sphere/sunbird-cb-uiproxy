1c9406745f6fa6576d91f31df1cfcf3304ffdce9
Sumit Bajaj <sumit.bajaj@tarento.com>
2024-06-20 14:15:40 +0530

    error page fix

--- src/admin/selfServicePortal.ts
+++ src/admin/selfServicePortal.ts
@@ -1,26 +1,59 @@
 import axios from 'axios'
 import { Router } from 'express'
-import fs from 'fs'
-import path from 'path'
-import util from 'util'
+
 import { CONSTANTS } from '../utils/env'
 import { logInfo } from '../utils/logger'
 export const adminApiV8 = Router()
-const getErrorPage = async () => {
-    const readFile = util.promisify(fs.readFile)
-    const filePath = path.join(__dirname, 'error.html')
-    return readFile(filePath, 'utf8')
-}
 const API_END_POINTS = {
     getLeanerCompletedResourceDetails: `${CONSTANTS.SELF_SERVICE_PORTAL_API_BASE}/v1/user/getLeanerCompletedResourceDetails`,
     getLearnerDetails: `${CONSTANTS.SELF_SERVICE_PORTAL_API_BASE}/v1/user/getLearnerDetails`,
 }
+const errorMessage = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Unauthorized Access</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      background-color: #f8f9fa; /* Light grey background */
+    }
+    .message-box {
+      text-align: center;
+      background-color: #fff;
+      padding: 30px;
+      border-radius: 10px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    .message-box h1 {
+      color: #ff0000; /* Red color for the error message */
+      margin-bottom: 10px;
+    }
+    .message-box p {
+      color: #333;
+    }
+  </style>
+</head>
+<body>
+  <div class="message-box">
+    <h1>Unauthorized Access</h1>
+    <p>You are not authorized to view this page or something went wrong. Please try again later.</p>
+  </div>
+</body>
+</html>
+`
 adminApiV8.get('/v1/user/getLearnerDetails', async (req, res) => {
     try {
         const { authorizationKey } = req.query
         const selfServiceAuthKey = CONSTANTS.SELF_SERVICE_PORTAL_AUTH_KEY
         if (!authorizationKey || (authorizationKey != selfServiceAuthKey)) {
-            return res.status(403).send(await getErrorPage())
+            return res.status(403).send(errorMessage)
         }
         const response = await axios({
             method: 'GET',
@@ -29,8 +62,8 @@ adminApiV8.get('/v1/user/getLearnerDetails', async (req, res) => {
         })
         res.status(200).send(response.data)
     } catch (error) {
-        logInfo(error)
-        return res.status(403).send(await getErrorPage())
+        logInfo(JSON.stringify(error))
+        return res.status(403).send(errorMessage)
     }
 })
 adminApiV8.get('/v1/user/getLeanerCompletedResourceDetails', async (req, res) => {
@@ -38,7 +71,7 @@ adminApiV8.get('/v1/user/getLeanerCompletedResourceDetails', async (req, res) =>
         const { authorizationKey } = req.query
         const selfServiceAuthKey = CONSTANTS.SELF_SERVICE_PORTAL_AUTH_KEY
         if (!authorizationKey || (authorizationKey != selfServiceAuthKey)) {
-            return res.status(403).send(await getErrorPage())
+            return res.status(403).send(errorMessage)
         }
         const response = await axios({
             method: 'GET',
@@ -47,7 +80,7 @@ adminApiV8.get('/v1/user/getLeanerCompletedResourceDetails', async (req, res) =>
         })
         res.status(200).send(response.data)
     } catch (error) {
-        logInfo(error)
-        return res.status(403).send(await getErrorPage())
+        logInfo(JSON.stringify(error))
+        return res.status(403).send(errorMessage)
     }
 })
