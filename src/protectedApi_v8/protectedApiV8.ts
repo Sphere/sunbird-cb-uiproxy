feac4a1958c0abd1f2522ffb7d2d58567127e4e4
likhithgowda <likhith.thammegowda@tarento.com>
2024-11-12 15:39:48 +0530

    Added Rc certificate API

--- src/protectedApi_v8/protectedApiV8.ts
+++ src/protectedApi_v8/protectedApiV8.ts
@@ -43,6 +43,7 @@ import { socialApi } from './social'
 import { trainingApi } from './training'
 import { translateApi } from './translate'
 import { updateProgressv2 } from './updateProgressv2'
+import { rcCert } from './user/rc-certificate'
 import { user } from './user/user'
 import { userEnrolledInSource } from './userEnrolledInSource'
 import { workAllocationApi } from './workallocation'
@@ -103,3 +104,4 @@ protectedApiV8.use('/creatorCertificateTemplate', creatorCertificateTemplate)
 protectedApiV8.use('/userEnrolledInSource', userEnrolledInSource)
 protectedApiV8.use('/ratings', ratingServiceApi)
 protectedApiV8.use('/AI', aiServiceAPI)
+protectedApiV8.use('/rcCert', rcCert)
