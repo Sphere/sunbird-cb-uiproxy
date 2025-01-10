76c44eef50a9b860ca0a5fa8f3813af5d2cf45c3
Sumit Bajaj <sumit.bajaj@tarento.com>
2025-01-10 11:50:29 +0530

    origin cors added

--- src/server.ts
+++ src/server.ts
@@ -59,7 +59,8 @@ export class Server {
   protected io = new SocketServer(this.httpServer, {
     cors: {
       methods: ['GET', 'POST'],
-      origin: '*',
+      origin: `${CONSTANTS.HTTPS_HOST}`,
+      credentials: true,
     },
   })
   // tslint:disable-next-line: no-any
