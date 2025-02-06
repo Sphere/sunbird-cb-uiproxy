b49105d102a11b8b7788263b3eae805156cde8da
Sumit Bajaj <sumit.bajaj@tarento.com>
2025-02-06 12:15:29 +0530

    build fix

--- src/server.ts
+++ src/server.ts
@@ -61,8 +61,8 @@ export class Server {
   protected io = new SocketServer(this.httpServer, {
     cors: {
       credentials: true,
-      methods: ['GET', 'POST', "OPTIONS"],
-      origin: "*",
+      methods: ['GET', 'POST', 'OPTIONS'],
+      origin: '*',
     },
   })
   // tslint:disable-next-line: no-any
