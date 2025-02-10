9a6e71d576081a92edbfd26616a5d39703c1340a
Sumit Bajaj <sumit.bajaj@tarento.com>
2025-02-10 15:54:44 +0530

    mobile app api changes

--- src/publicApi_v8/mobileAppApi.ts
+++ src/publicApi_v8/mobileAppApi.ts
@@ -80,7 +80,7 @@ export const mobileAppApi = Router()
 const verifyToken = (req: any, res: any) => {
   try {
     logInfo('Inside verify token function')
-    const accessToken = req.headers[authenticatedToken] || extractUserToken(req)
+    const accessToken = req.headers[authenticatedToken]
     logInfo('Token via cookie', accessToken)
     // tslint:disable-next-line: no-any
     try {
