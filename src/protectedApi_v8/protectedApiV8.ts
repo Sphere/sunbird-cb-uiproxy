24a97c652f65029134167920e7ef8e6f153ea644
sumitbajaj-tarento <sumit.bajaj@tarento.com>
2025-02-18 10:11:14 +0530

    Update protectedApiV8.ts

--- src/protectedApi_v8/protectedApiV8.ts
+++ src/protectedApi_v8/protectedApiV8.ts
@@ -47,6 +47,7 @@ import { user } from './user/user'
 import { userEnrolledInSource } from './userEnrolledInSource'
 import { workAllocationApi } from './workallocation'
 import { workflowHandlerApi } from './workflow-handler'
+import { sunbirdrRcCertificate } from './rcEvents'
 export const protectedApiV8 = express.Router()
 
 protectedApiV8.get('/', (_req, res) => {
@@ -104,3 +105,4 @@ protectedApiV8.use('/userEnrolledInSource', userEnrolledInSource)
 protectedApiV8.use('/ratings', ratingServiceApi)
 protectedApiV8.use('/AI', aiServiceAPI)
 protectedApiV8.use('/rcCert', rcCert)
+protectedApiV8.use('/sunbirdrRcCertificate', sunbirdrRcCertificate)
