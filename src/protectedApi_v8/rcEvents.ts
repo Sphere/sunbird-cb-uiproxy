b5de1331ebea130b22c38f56cfe7f48b33669274
Sumit Bajaj <sumit.bajaj@tarento.com>
2025-03-05 17:15:59 +0530

    eventid updates

--- src/protectedApi_v8/rcEvents.ts
+++ src/protectedApi_v8/rcEvents.ts
@@ -46,7 +46,7 @@ sunbirdrRcCertificate.post('/events', async (req, res) => {
 
 sunbirdrRcCertificate.post('/events/edit', async (req, res) => {
     logInfo('Edit event request body', req.body)
-    const { eventId, eventName, eventDescription, eventDate, eventPlace, eventType, updatedBy } = req.body
+    const { eventId, eventName, eventDescription, eventDate, eventPlace, eventType, templateId, updatedBy } = req.body
 
     // Check if the required eventId is provided and exists
     if (!eventId) {
@@ -67,6 +67,7 @@ sunbirdrRcCertificate.post('/events/edit', async (req, res) => {
             eventName: eventName || result.rows[0].eventName,
             eventPlace: eventPlace || result.rows[0].eventPlace,
             eventType: eventType || result.rows[0].eventType,
+            templateId: templateId || result.rows[0].templateId,
             updatedAt: new Date(),
             updatedBy,
         }
@@ -74,7 +75,7 @@ sunbirdrRcCertificate.post('/events/edit', async (req, res) => {
         // Construct the update query
         const updateQuery = `
             UPDATE sunbird.rc_events
-            SET eventName = ?, eventDescription = ?, eventDate = ?, eventPlace = ?, eventType = ?, updatedBy = ?, updatedAt = ?
+            SET eventName = ?, eventDescription = ?, eventDate = ?, eventPlace = ?, eventType = ?, updatedBy = ?, updatedAt = ?,templateId = ?
             WHERE eventId = ?
         `
         const updateParams = [
@@ -85,6 +86,7 @@ sunbirdrRcCertificate.post('/events/edit', async (req, res) => {
             updatedFields.eventType,
             updatedFields.updatedBy,
             updatedFields.updatedAt,
+            updatedFields.templateId,
             eventId,
         ]
 
