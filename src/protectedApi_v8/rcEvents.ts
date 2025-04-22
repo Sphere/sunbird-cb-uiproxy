4b23ab790840cb843b18f216ebd2a3e351b4c3bf
sumitbajaj-tarento <sumit.bajaj@tarento.com>
2025-04-22 14:21:27 +0530

    Update rcEvents.ts

--- src/protectedApi_v8/rcEvents.ts
+++ src/protectedApi_v8/rcEvents.ts
@@ -141,6 +141,7 @@ sunbirdrRcCertificate.get('/events', async (_req, res) => {
             eventName: event.eventname,
             eventPlace: event.eventplace,
             eventType: event.eventtype,
+            status: event.status,
             templateId: event.templateid,
             updatedAt: event.updatedat,
             updatedBy: event.updatedby,
