11edd23cacaf57391620130805262356ddbdccc9
princegupta1131 <114015020+princegupta1131@users.noreply.github.com>
2025-08-08 16:23:22 +0530

    remvoed double quots to single

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -1619,7 +1619,7 @@ export const API_LIST = {
     '/proxies/v8/learner/course/v1/enrol',
     '/proxies/v8/learnervm/private/content/v3/retire/',
     '/proxies/v8/private/content/v3/update/:do_id',
-    "/proxies/v8/fractv1",
+    '/proxies/v8/fractv1',
     '/protected/v8/user/profileDetails/test',
     '/protected/v8/resource/',
     '/protected/v8/user/details',
