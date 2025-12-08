f6c70dbb058cb700e54a0391265e993a35acf011
princegupta1131 <114015020+princegupta1131@users.noreply.github.com>
2025-12-08 15:42:51 +0530

    Added unmatch sunbird password for user create

--- src/protectedApi_v8/rcEvents.ts
+++ src/protectedApi_v8/rcEvents.ts
@@ -4,9 +4,10 @@ import axios from 'axios'
 import cassandra from 'cassandra-driver'
 import { Router } from 'express'
 import uuid from 'uuid'
-import { encryptData } from '../utils/emailHashPasswordGenerator'
+// import { encryptData } from '../utils/emailHashPasswordGenerator'
 import { CONSTANTS } from '../utils/env'
 import { logError, logInfo } from '../utils/logger'
+import { getRCPassword } from '../utils/rcPasswordGenerator'
 
 const s3 = new AWS.S3({
     accessKeyId: CONSTANTS.RC_S3_ACCESS_KEY_ID,
@@ -506,8 +507,8 @@ const checkIfuserExists = async (phone: string) => {
             let lastName = userData.lastName || ''
 
             if (userData.profileDetails && userData.profileDetails.profileReq && userData.profileDetails.profileReq.personalDetails) {
-                firstName = userData.profileDetails.profileReq.personalDetails.firstname || firstName
-                lastName = userData.profileDetails.profileReq.personalDetails.surname || userData.profileDetails.profileReq.personalDetails.firstname || lastName
+                firstName = userData.profileDetails.profileReq.personalDetails.firstname || firstName // fallback to top-level firstName
+                lastName = userData.profileDetails.profileReq.personalDetails.surname || userData.profileDetails.profileReq.personalDetails.firstname || lastName // fallback to top-level lastName or firstName
             }
 
             const userResult = {
@@ -541,7 +542,7 @@ const createUserIfNotExists = async (userData: any) => {
             request: {
                 firstName: userData.firstName,
                 lastName: userData.lastName || `${userData.firstName}`,
-                password: encryptData(userData.phone),
+                password: getRCPassword(userData), // Uses encrypted password if valid; otherwise generates a Sunbird-compliant fallback password to avoid UOS_USRCRT0024 failure
                 phone: userData.phone,
             },
         }
