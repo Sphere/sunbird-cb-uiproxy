dce6ec2cf803b13e97b1485263b43990500e6d47
princegupta1131 <114015020+princegupta1131@users.noreply.github.com>
2025-12-22 11:49:45 +0530

    fix typo error

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -1553,7 +1553,7 @@ export const API_LIST = {
       ],
     },
     '/protected/v8/playlist/search': {
-      chainsNeeded: [CHECK.ROLE],
+      checksNeeded: [CHECK.ROLE],
       // tslint:disable-next-line: object-literal-sort-keys
       ROLE_CHECK: [ROLE.PUBLIC],
     },
