8e3919b57242acaacf94c86052a0a031114ef579
likhithgowda <likhith.thammegowda@tarento.com>
2026-01-20 11:29:54 +0530

    whitelist leaderboard API

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -1829,6 +1829,7 @@ export const API_LIST = {
     '/proxies/v8/action/framework/v3/category/retire/:id',
     '/proxies/v8/action/framework/v3/term/create',
     '/proxies/v8/user/v1/passbook',
+    '/proxies/v8/user/v1/leaderboard',
     '/proxies/v8/user/v1/admin/passbook',
     '/public/v8/publicTelemetry',
     '/public/v8/publicTelemetry/telemetry',
